6a9907d786f2aa677595523df2a0e09ca35e5050
nmsn <136696700@qq.com>
2023-04-10 17:12:00 +0800

    refactor: format

--- src/index.js
+++ src/index.js
@@ -1,38 +1,16 @@
-import fs from "fs";
 import path from "path";
 import process from "process";
 import { fileURLToPath } from "url";
-import mustache from "mustache";
-import {
-  getTemplateName,
-  selectTemplate,
-  editTemplate,
-  getOutputName,
-} from "./util.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const __curDir = process.cwd();
+import { getOutputFileName, getTpl, saveFile } from "./util.js";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const tplDir = path.join(__dirname, "template");
 
-const getOutput = async (template) => {
-  const tpl = fs.readFileSync(`${tplDir}/${template}`, "utf-8");
-  const slots = tpl.match(/(?<=\{\{\{)(\S+?)(?=\}\}\})/gm);
-  const result = await editTemplate([...new Set(slots)]);
-  const output = mustache.render(tpl, result);
-  return output;
-};
-
-const saveFile = (filename, output) => {
-  fs.writeFileSync(`${__curDir}/${filename}`, output);
-};
-
 const cli = async () => {
-  const files = await getTemplateName(tplDir);
-  const { template } = await selectTemplate(files);
-  const output = await getOutput(template);
-  const fileName = await getOutputName();
+  const output = await getTpl(tplDir);
+  const fileName = await getOutputFileName();
   saveFile(fileName, output);
 };
 
--- src/util.js
+++ src/util.js
@@ -1,23 +1,9 @@
 import fs from "fs";
 import inquirer from "inquirer";
+import mustache from "mustache";
+import process from "process";
 
-export const getTemplateName = async (path) => {
-  const files = fs.readdirSync(path);
-  return files;
-};
-
-export const selectTemplate = async (fileNames) => {
-  const result = await inquirer.prompt([
-    {
-      type: "list",
-      name: "template",
-      message: "Select the template of code:",
-      choices: fileNames.map((item) => ({ value: item, name: item })),
-    },
-  ]);
-
-  return result;
-};
+const __curDir = process.cwd();
 
 export const editTemplate = async (names) => {
   const result = await inquirer.prompt(
@@ -31,12 +17,35 @@ export const editTemplate = async (names) => {
   return result;
 };
 
-export const getOutputName = async () => {
-  const result = await inquirer.prompt({
+export const getTpl = async (path) => {
+  const files = fs.readdirSync(path);
+
+  const { template: sourceTpl } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "template",
+      message: "Select the template of code:",
+      choices: files.map((item) => ({ value: item, name: item })),
+    },
+  ]);
+
+  const tpl = fs.readFileSync(`${path}/${sourceTpl}`, "utf-8");
+  const slots = tpl.match(/(?<=\{\{\{)(\S+?)(?=\}\}\})/gm);
+  const result = await editTemplate([...new Set(slots)]);
+  const output = mustache.render(tpl, result);
+  return output;
+};
+
+export const getOutputFileName = async () => {
+  const { name } = await inquirer.prompt({
     type: "input",
     name: "name",
     message: `Input output file name:`,
   });
 
-  return result.name;
+  return name;
+};
+
+export const saveFile = (filename, output) => {
+  fs.writeFileSync(`${__curDir}/${filename}`, output);
 };
